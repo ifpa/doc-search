991f98ddd991b60213cacb1acfb28eb275082de6
Phillip Copley <phillip.copley@gmail.com>
2015-01-03 15:12:08 -0500

    Testing if GHP will load PHP index page

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+    $var = "test";
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Insurance Federation of PA Member's Area</title>
+</head>
+<body>
+    <h1>Member's Area</h1>
+    <p><?=$var;?></p>
+</body>
+</html>
