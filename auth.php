b1bcc36ec8e68bc94be11c456ee4b1a8e269c76c
Phillip Copley <phillip.copley@gmail.com>
2015-01-04 18:58:46 -0500

    Fake auth

--- auth.php
+++ auth.php
@@ -2,10 +2,12 @@
 
 session_start();
 
-if ($_POST['username'] != '' and $_POST['password'] != '') {
+$_SESSION['logged_in'] = true;
+
+/*if ($_POST['username'] != '' and $_POST['password'] != '') {
     $_SESSION['logged_in'] = true;
     $_SESSION['ifp'] = (strpos($_POST['username'], '@ifpenn.org') !== false) ? true : false;
-}
+}*/
 
 header('Location:http://104.131.60.233/pls.php');
 die();
